db10d180614018456206a2bd316216e9cd57eff7
Alisson Dias de Oliveria <dias95357@gmail.com>
2026-01-05 14:40:31 -0300

    aulas sobre json
    Converter json para um dicionário
    Salvar um dicionário como um json

---
+++ modulos/aula175_176.py
@@ -0,0 +1,66 @@
+# O que é JSON - JavaScript Object Notation
+# JSON - JavaScript Object Notation (extensão .json)
+# É uma estrutura de dados que permite a serialização de objetos em texto simples para facilitar a transmissão de dados através da rede, APIs web ou outros meios de comunicação.
+# O JSON suporta os seguintes tipos de dados:
+# Números: podem ser inteiros ou com ponto flutuante, como 42 ou 3.14
+# Strings: são cadeias de caracteres, como "Olá, mundo!" ou "12345"
+# As strings devem ser envolvidas por aspas duplas
+# Booleanos: são os valores verdadeiro (true) ou falso (false)
+# Arrays: são listas ordenadas de valores, como [1, 2, 3] ou ["Oi", "Olá", "Bom dia"]
+# Objetos: são conjuntos de pares chave/valor -> {"nome": "João", "idade": 30}
+# null: é um valor especial que representa ausência de valor
+#
+# Ao converter de Python para JSON:
+# Python        JSON
+# dict          object
+# list, tuple   array
+# str           string
+# int, float    number
+# True          true
+# False         false
+# None          null
+
+import json
+
+str_json = '''
+{
+  "title": "O Senhor dos Anéis: A Sociedade do Anel",
+  "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+  "is_movie": true,
+  "imdb_rating": 8.8,
+  "year": 2001,
+  "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+  "budget": null
+}
+
+'''
+print(str_json) # imprime em formato json
+
+filme = json.loads(str_json) # converte o json para um dicionário
+
+class Movie ():
+  title: str
+  original_title: str
+  is_movie: bool
+  imdb_rating: float
+  year: int
+  characters: list[str]
+  budget: None
+
+str_json2 = '''
+{
+  "title": "O Senhor dos Anéis: A Sociedade do Anel",
+  "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+  "is_movie": true,
+  "imdb_rating": 8.8,
+  "year": 2001,
+  "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+  "budget": null
+}
+'''
+
+filme2: Movie = json.loads(str_json) # filme2 é do tipo Movie e herdou todas os seus atributos
+print(filme2['title'])
+
+print(json.dumps(filme, ensure_ascii=False, indent=2)) 
+#json.dumps remove os acentos do texto, ensure_ascii=False, força a mostrar os acentos, indent é a indentação
