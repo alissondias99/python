2aa49a7447c40057d99f189ad34acb7fed86c961
Alisson Dias de Oliveria <dias95357@gmail.com>
2026-01-20 17:15:35 -0300

    seguindo no projeto calculadora + renomeando arquivos antigos

--- modulos/aula202_calculadora/main.py
+++ modulos/aula202_calculadora/main.py
@@ -1,12 +1,15 @@
 import sys
 
+from buttons import ButtonsGrid
+from display import Display
+from info import Info
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
+from styles import setupTheme
 from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
-    
     # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
@@ -15,6 +18,18 @@ if __name__ == '__main__':
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # Info
+    info = Info('2.0 ^ 10.0 = 1024')
+    window.addWidgetToVLayout(info)
+
+    # Display
+    display = Display()
+    window.addWidgetToVLayout(display)
+
+    # Grid
+    buttonsGrid = ButtonsGrid(display)
+    window.vLayout.addLayout(buttonsGrid)
 
     # Executa tudo
     window.adjustFixedSize()
--- modulos/aula202_calculadora/main_window.py
+++ modulos/aula202_calculadora/main_window.py
@@ -1,13 +1,12 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel
-
+from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QLabel
 
 class MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
        
         self.cw = QWidget()
-        self.v_layout = QVBoxLayout()
-        self.cw.setLayout(self.v_layout)
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
         self.setCentralWidget(self.cw)
 
         self.setWindowTitle('Calculadora')
@@ -16,6 +15,5 @@ class MainWindow(QMainWindow):
         self.adjustSize() # a janela vai se adaptar aos textos dentro dela
         self.setFixedSize(self.width(), self.height()) # não será possivel alterar a altura ou largura da janela
 
-
-    def addWidgetToLayout(self, widget: QWidget): # boa pratica que diminui a quantidade de níveis em window.adjustFixedSize() do main.py
-        self.v_layout.addWidget(widget)
+    def addWidgetToVLayout(self, widget: QWidget): # boa pratica que diminui a quantidade de níveis em window.adjustFixedSize() do main.py
+        self.vLayout.addWidget(widget)
