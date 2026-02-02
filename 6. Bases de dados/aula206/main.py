9159c6c60d89f9d0b061f303fe3d733428f8e3fb
Alisson Dias de Oliveria <dias95357@gmail.com>
2026-02-02 16:26:54 -0300

    aulas sobre docker e conexões com banco de dados mysql com pymysql

---
+++ 6. Bases de dados/aula206/main.py
@@ -0,0 +1,23 @@
+# PyMySQL - um cliente MySQL feito em Python Puro
+# pip install pymysql
+# Doc: https://pymysql.readthedocs.io/en/latest/
+# Pypy: https://pypi.org/project/pymysql/
+# GitHub: https://github.com/PyMySQL/PyMySQL
+
+import os
+import pymysql
+import dotenv
+
+dotenv.load_dotenv() # recuperar as variaveis de ambiente diretamente do arquivo .env que não sendo ignorado pelo .gitignore
+
+conn = pymysql.connect(
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
+)
+
+with conn:
+    with conn.cursor() as cursor:
+        # SQL
+        print(cursor)
