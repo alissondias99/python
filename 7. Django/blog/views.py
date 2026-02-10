a318dcd154d7567629bf71ee5221d0fd9857a8c6
Alisson Dias de Oliveria <dias95357@gmail.com>
2026-02-10 14:14:54 -0300

    criando css global, renomeando arquivos, linkando páginas com menu de navegação e atualizando arquivos que não são mias úteis

--- 7. Django/blog/views.py
+++ 7. Django/blog/views.py
@@ -6,7 +6,10 @@ def blog(request):
     print('blog')
     return render(
         request,
-        'blog/blog.html'
+        'blog/blog.html',
+        {
+            'text': 'Estamos no blog' # ← context, define comportamentos da página
+        }
     )
 
 
@@ -14,5 +17,8 @@ def exemplo(request):
     print('exemplo')
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        {
+            'text': 'Estamos no exemplo' # ← context, define comportamentos da página
+        }
     )
--- 7. Django/home/views.py
+++ 7. Django/home/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    print('home')
     return render(
         request, 
-        'home/home.html' # template name requerido pelo request
+        'home/index.html', # template name requerido pelo request, vai recuperar os dados de base->global->base.html
+        {
+            'text': 'Estamos na home' # ← context, define comportamentos da página
+        }
     )
