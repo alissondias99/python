a6a0658b5ecea7df1c3d5b9cc970ff3a384aa03a
Alisson Dias de Oliveria <dias95357@gmail.com>
2026-03-02 11:57:41 -0300

    melhorando o projeto Blog
    * A barra de pesquisa agora está totalmente funcional.
    * Implementada a paginação para facilitar a visualização da lista.
    * Adicionada a estrutura visual do formulário de cadastro de contatos (funcionalidade ainda em desenvolvimento).

--- 8. Projeto_agenda/contact/urls.py
+++ 8. Projeto_agenda/contact/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-
 from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+
+    # CRUD do contact
+    path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/detail', views.contact, name='contact'),
 ]
--- 8. Projeto_agenda/contact/views/contact_views.py
+++ 8. Projeto_agenda/contact/views/contact_views.py
@@ -1,19 +1,54 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.core.paginator import Paginator
 from contact.models import Contact
 
 def index(request):
 
-    # somente contatos com show marcado iram aparecer na página
+    # somente contatos com show = True iram aparecer na página
     contacts = Contact.objects \
         .filter(show=True) \
         .order_by('-id') 
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     return render(
         request,
         'contact/index.html',
         {
-            'contacts': contacts,
+            'page_obj': page_obj,
             'title': 'Contatos -',
+        }
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '': # barra de pesquisa vazia = redireciona para a base
+        return redirect('contact:index')
+    
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(
+                Q(first_name__icontains = search_value,) |
+                Q(last_name__icontains = search_value,) |
+                Q(phone__icontains = search_value,) |
+                Q(email__icontains = search_value,)
+                ) \
+        .order_by('-id') 
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'contact/index.html',
+        {
+            'page_obj': page_obj,
+            'title': 'Search -',
         }
     )
 
