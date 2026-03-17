7c42cf652c31f212d9ee730b4157b3bc8ba064f9
Alisson Dias de Oliveria <dias95357@gmail.com>
2026-03-17 16:03:54 -0300

    Novas funcionalidades
    Agora os usuários podem atualizar e deletar contatos.
    Foi adicionado um link para a página de criar contatos

--- 8. Projeto_agenda/contact/urls.py
+++ 8. Projeto_agenda/contact/urls.py
@@ -1,13 +1,18 @@
+# type:ignore
 from django.urls import path
+
 from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
 
-    # CRUD do contact
+    # contact (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
-    path('contact/<int:contact_id>/detail', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
 ]
